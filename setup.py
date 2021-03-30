2dbd8a18bcbfb9c79c518a324a1b0cea124ae829
Robert Henkhaus <robert.henkhaus@drillinginfo.com>
2021-03-30 13:34:12 -0500

    add install requirements, increment to v0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ahapy",
-    version="0.0.3",
+    version="0.0.4",
     author="Robert Henkhaus",
     author_email="rhenkhaus@gmail.com",
     description="A small Aha.io API client",
@@ -20,6 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=['requests>=2.22.0'],
     keywords=["aha", "aha.io"],
     packages=setuptools.find_packages(exclude=('test*', )),
     package_dir={'ahapy': 'ahapy'},
